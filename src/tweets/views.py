2b592fec061024f9d1c1a1f2722573aba171b279
AleksandrPeshcherov <kawalot@gmail.com>
2017-10-22 21:52:25 +0300

    CRUD & first views

--- src/tweets/views.py
+++ src/tweets/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
+from .models import Tweet
 # Create your views here.
+
+def tweet_detail_view(request, id=1):
+    obj = Tweet.objects.get(id=id)
+    print(obj)
+    context = {
+        "object": obj
+    }
+    return render(request, "tweets/detail_view.html", context)
+
+def tweet_list_view(request):
+    queryset = Tweet.objects.all()
+    print(queryset)
+    context = {
+        "object_list": queryset
+    }
+    return render(request, "tweets/list_view.html", context)
