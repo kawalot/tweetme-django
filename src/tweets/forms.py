0e227739983adf514dabbb701c6e5638cdc56bd2
AleksandrPeshcherov <kawalot@gmail.com>
2017-11-02 22:25:33 +0200

    Django Crispy Forms

--- src/tweets/forms.py
+++ src/tweets/forms.py
@@ -3,7 +3,12 @@ from django import forms
 from .models import Tweet
 
 class TweetModelForm(forms.ModelForm):
-    content = forms.CharField(label='', widget=forms.Textarea(attrs={'placeholder': "Your message", "class": "form-control"}))
+    content = forms.CharField(label='', 
+                widget=forms.Textarea(
+                    attrs={'placeholder': "Your message", 
+                            "class": "form-control"}
+                            )
+                )
     class Meta:
         model = Tweet
         fields = [
