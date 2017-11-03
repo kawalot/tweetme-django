b141b1b1336f74a5973fbaa0a938df7d82417de3
AleksandrPeshcherov <kawalot@gmail.com>
2017-11-03 22:17:56 +0200

    API Serializer & View

--- src/tweets/api/urls.py
+++ src/tweets/api/urls.py
@@ -1,17 +1,11 @@
 from django.conf.urls import url
 
 from django.views.generic.base import RedirectView
-from .views import (
-    # TweetDeleteView, 
-    # TweetDetailView, 
-    # TweetListView, 
-    # TweetCreateView,
-    # TweetUpdateView
-    )
+from .views import TweetListAPIView
 
 urlpatterns = [
     # url(r'^$', RedirectView.as_view(url="/")),
-    # url(r'^search/$', TweetListView.as_view(), name='list'),
+    url(r'^$', TweetListAPIView.as_view(), name='list'), # /api/tweet/
     # url(r'^create/$', TweetCreateView.as_view(), name='create'),
     # url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name='detail'),
     # url(r'^(?P<pk>\d+)/update/$', TweetUpdateView.as_view(), name='update'), 
