40f30c2072fc94674beac8602b5800ff8a61ef83
AleksandrPeshcherov <kawalot@gmail.com>
2017-11-07 22:16:24 +0200

    User Detail View

--- src/tweets/api/views.py
+++ src/tweets/api/views.py
@@ -3,7 +3,7 @@ from rest_framework import permissions
 from tweets.models import Tweet
 from .serializers import TweetModelSerializer
 from django.db.models import Q
-
+from .pagination import StandartResultsPagination
 
 
 class TweetCreateAPIView(generics.CreateAPIView):
@@ -15,6 +15,7 @@ class TweetCreateAPIView(generics.CreateAPIView):
 
 class TweetListAPIView(generics.ListAPIView):
     serializer_class = TweetModelSerializer
+    pagination_class = StandartResultsPagination
 
     def get_queryset(self, *args, **kwargs):
         qs = Tweet.objects.all().order_by("-timestamp")
