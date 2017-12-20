4e3b900bdb81e85fc18bd4cc3caf88d6609c131e
AleksandrPeshcherov <kawalot@gmail.com>
2017-12-20 22:55:34 +0200

    Single Detail Tweet

--- src/tweets/api/urls.py
+++ src/tweets/api/urls.py
@@ -1,12 +1,18 @@
 from django.conf.urls import url
 
 from django.views.generic.base import RedirectView
-from .views import TweetListAPIView, TweetCreateAPIView, RetweetAPIView, LikeToggleAPIView
+from .views import (TweetListAPIView, 
+                    TweetCreateAPIView, 
+                    RetweetAPIView, 
+                    LikeToggleAPIView, 
+                    TweetDetailAPIView
+                )
 
 urlpatterns = [
     # url(r'^$', RedirectView.as_view(url="/")),
     url(r'^$', TweetListAPIView.as_view(), name='list'), # /api/tweet/
     url(r'^create/$', TweetCreateAPIView.as_view(), name='create'),
+    url(r'^(?P<pk>\d+)/$', TweetDetailAPIView.as_view(), name='detail'),
     url(r'^(?P<pk>\d+)/like/$', LikeToggleAPIView.as_view(), name='like-toggle'),
     url(r'^(?P<pk>\d+)/retweet/$', RetweetAPIView.as_view(), name='retweet'),
     # url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name='detail'),
