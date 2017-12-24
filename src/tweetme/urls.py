686f448e6a2f7f0f5f1ccd59603f21a421915204
AleksandrPeshcherov <kawalot@gmail.com>
2017-12-25 00:05:23 +0200

    Registration & Auth

--- src/tweetme/urls.py
+++ src/tweetme/urls.py
@@ -23,15 +23,18 @@ from hashtags.views import HashTagView
 from tweets.views import TweetListView
 from .views import home, SearchView
 from hashtags.api.views import TagTweetAPIView
+from accounts.views import UserRegisterView
 
 urlpatterns = [
+    url(r'^register/$', UserRegisterView.as_view(), name='register'),
     url(r'^admin/', admin.site.urls),
     url(r'^$', TweetListView.as_view(), name='home'),
     url(r'^search/$', SearchView.as_view(), name='search'),
     url(r'^tags/(?P<hashtag>.*)/$', HashTagView.as_view(), name='hashtag'),
     url(r'^tweet/', include('tweets.urls', namespace='tweet')),
     url(r'^api/tweet/', include('tweets.api.urls', namespace='tweet-api')),
-    url(r'^api/', include('accounts.api.urls', namespace='profiles-api')),    
+    url(r'^api/', include('accounts.api.urls', namespace='profiles-api')),
+    url(r'^', include('django.contrib.auth.urls')),   
     url(r'^', include('accounts.urls', namespace='profiles')),
     url(r'^api/search/$', SearchTweetAPIView.as_view(), name='search-api'),
     url(r'^api/tags/(?P<hashtag>.*)/$', TagTweetAPIView.as_view(), name='tag-tweet-api'),
